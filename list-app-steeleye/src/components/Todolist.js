85ce793bbe04008e7c8cd41209178c772443623e
sameerapk <sameerkmurthy@gmail.com>
2021-08-21 19:51:22 +0530

    handling empty todolist condition

--- list-app-steeleye/src/components/Todolist.js
+++ list-app-steeleye/src/components/Todolist.js
@@ -4,14 +4,14 @@ import Todo from './Todo'
 const Todolist = ({todos,delTodo, updTodo, textClicked}) => {
     return (
         <div>
-            {todos.map(item=>(
+            {todos.length ? todos.map(item=>(
                 <Todo 
                 key={item.id}
                 delTodo={delTodo}
                 updTodo={updTodo}
                 textClicked={textClicked}
                 item={item} />
-            ))}
+            )) : <h1 style={{color: '#fff'}}>No more todos left. Go ahead and add it!!!</h1>}
         </div>
     )
 }
